d0c95c8f40c902eddac3211ecd5e4c0536d4c3c6
Kur0a5h <kuroash.esmaili@gmail.com>
2019-01-14 12:26:54 -0800

    project setup in pp in progress

---
+++ src/components/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default props => (
+    <div>
+        <h1 className="center">Movie Quotes</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, culpa blanditiis. Rem quisquam itaque molestias ipsum odio, repellendus deserunt numquam necessitatibus amet cupiditate! Laboriosam illo iure deserunt nobis. Ea incidunt eius dolor, iusto officia tempora eaque ut dolores voluptatibus eligendi laboriosam esse repudiandae atque veniam mollitia vel animi, necessitatibus rerum, reprehenderit molestiae id. Dolore cupiditate quia eveniet ab ea corporis repellat, reprehenderit ducimus cumque fugiat facilis. Culpa perspiciatis ipsa porro in at ullam sit, eum sunt eos asperiores dolorem velit cum nulla architecto illo voluptatibus aut tempore inventore rerum repellendus ipsum quaerat consequuntur dolore. Facere molestias eum iste mollitia error.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi maxime adipisci iure labore maiores</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+    </div>
+)
