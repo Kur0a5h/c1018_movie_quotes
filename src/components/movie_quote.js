0cd0436610ed51a6039f72eaa984b53b4376d95f
Kur0a5h <kuroash.esmaili@gmail.com>
2019-01-14 17:02:57 -0800

    project setup complete

---
+++ src/components/movie_quote.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+export default class extends Component{
+    render(){
+        return(
+            <div className="center">
+            <h1>Movie Quote</h1>
+            <h5>Movie Quote: I haven't got time for this mickey mouse bullshit!</h5>
+            </div>
+        )
+    }
+}
+
